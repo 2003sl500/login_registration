278d709f0007cdf81cb696e9c1fedbfde77c4f8e
Daniel Shoemaker <danielshoe@charter.net>
2021-08-15 20:38:39 -0700

    Finished

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,8 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.registration import Register
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def base_route():
@@ -13,25 +15,51 @@ def registration():
 
 @app.route('/index/register', methods = ['POST'])
 def register():
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
     data = {
         "first_name": request.form['first_name'],
         "last_name": request.form['last_name'],
         "email": request.form['email'],
-        "password": request.form['password']
+        "password": pw_hash
     }
+
     if not Register.validate(request.form):
         return redirect('/index/register_form')
     
     user_info = Register.create(data)
-    print("************* user_info, controller, line 23, users id: ", user_info)
     session['id'] = user_info
     return redirect('/index/registration_conf')
 
 @app.route('/index/registration_conf')
 def registration_conf():
     reg_info = Register.show_some(session['id'])
+    session['id'] = ""
     return render_template("registration_conf.html", reg_info = reg_info)
 
 @app.route('/index/login')
 def login():
-    return render_template("login.html")
+    session['current_user_id'] = ""
+    return render_template('login.html')
+
+@app.route('/index/login/form', methods = ['POST'])
+def login_form():
+    data = {
+        "email": request.form['email'], 
+    }
+    user_found = Register.find_email(request.form['email'])
+    if not user_found:
+        flash("Invalid Email/Password")
+        return redirect('/index/register_form')
+    if not bcrypt.check_password_hash(user_found[0]['password'], request.form['password']):
+        print("*********** Invalid password")
+        flash("Invalid Email/Password")
+        return redirect('/index/login')
+    session['current_user_id'] = user_found[0]['id']
+    print("************* session['current_user_id]: ", session['current_user_id'])
+    return redirect('/index/login/conf')
+
+@app.route('/index/login/conf')
+def login_conf():
+    if session['current_user_id'] == "":
+        return redirect('/index/register_form')
+    return render_template('login_conf.html')
--- flask_app/models/registration.py
+++ flask_app/models/registration.py
@@ -1,6 +1,9 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 import re
+from flask_app.controllers.users import app
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 DATABASE = 'reg'
 TABLE1 = 'personal_info'
@@ -18,7 +21,6 @@ class Register:
     def get_all(cls):
         query = f'SELECT * FROM {TABLE1};'
         results = connectToMySQL(DATABASE).query_db(query)
-        print("************* RESULTS: ", results)
         information = []
         for info in results:
             information.append( cls(info) )
@@ -31,7 +33,6 @@ class Register:
             'id': id
         }
         results = connectToMySQL(DATABASE).query_db(query, data)
-        print("************* show_some(), results: ", results)
         information = []
         for info in results:
             information.append( cls(info) )
@@ -39,31 +40,48 @@ class Register:
 
     @classmethod
     def create(cls, data):
+        # pw = bcrypt.generate_password_hash(data['password'])
         query = f'INSERT INTO {TABLE1}( first_name, last_name, email, password, updated_at ) VALUES( %(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW() );'
         return connectToMySQL(DATABASE).query_db(query, data)
     
+    @classmethod
+    def find_email(cls, email):
+        query = f'SELECT id, password FROM {TABLE1} WHERE email = %(email)s;'
+        data = {
+            'email': email
+        }
+        return connectToMySQL(DATABASE).query_db(query, data)
+
     @staticmethod
     def validate(form_data):
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_i]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         is_valid = True
         if (form_data['first_name'] == "") or (len(form_data['first_name']) < 3):
-            print("********** registration.py, line 50")
+            print("********** registration.py, line 59")
             flash("First name must be at least 3 characters")
             is_valid = False
         if (form_data['last_name'] == "") or (len(form_data['last_name']) < 3):
-            print("********** registration.py, line 54")
+            print("********** registration.py, line 63")
             flash("Last name must be at least 3 characters")
             is_valid = False
         if (form_data['email'] == ""):
-            print("********** registration.py, line 58")
+            print("********** registration.py, line 67")
             flash("Email is required")
             is_valid = False
-        elif (EMAIL_REGEX.match(form_data['email'])):
-            print("********** registration.py, line 61")
-            return is_valid
-        else:
-            print("********** registration.py, line 64")
+        if Register.find_email(form_data['email']):
+            flash("You are already registered")
             is_valid = False
             return is_valid
+        elif not (EMAIL_REGEX.match(form_data['email'])):
+            print("********** registration.py, line 75")
+            is_valid = False
+        if len(form_data['password']) < 8:
+            flash("Password must be at least 8 characters long")
+            print("************* password length: ", len(form_data['password']))
+            is_valid = False
+        elif (form_data['password']) != (form_data['conf_password']):
+            print("********** password does not match conf_password")
+            flash("Password does not match Confirm Password")
+            is_valid = False
 
-        return
+        return is_valid
